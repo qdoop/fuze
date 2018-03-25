41c2cd0b6984a9b245de5343dcaa77d5e3c92ac8
qdoop <qdoop@outlook.com>
2018-03-25 22:23:52 +0300

    Gulp API

--- src/Bootstrap/Main.fs
+++ src/Bootstrap/Main.fs
@@ -7,8 +7,9 @@ namespace Fuze
 
 [<AutoOpen>]
 module Main=
-
+    open System
     open Fuze.Gulp
+    
     
     // [<EntryPoint>]
     let main vargs = 
@@ -19,18 +20,66 @@ module Main=
         vargs |> zlog
 
 
-        let zgulp=Fuze.Gulp.Gulp()
-        zgulp.task "task0" ["*.fs", "!*.fsx"] ( fun cxt ->
+        // let zgulp=Fuze.Gulp.Gulp()
+        zgulp.task ("task0", ["*.fs"; "!*.fsx"],  fun cxt ->
             printfn "fun %A" cxt 
 
-            zgulp.src(["x"]).pipe(
-                zgulp.dest ["yyy"]
-            )
+            cxt       
+            + zgulp . src ["x"]
+            + zgulp.dst ["yyy"]
+            + zgulp.src ["x"]
+            + zgulp.dst ["yyy"]
+
+        )
 
 
-            ()
-            )
+        zgulp.task ("task0", ["*.fs"; "!*.fsx"])
+
+        zgulp.task ("task0", ["*.fs"; "!*.fsx"], [], fun cxt ->
         
+            cxt
+        )
+        
+
+
+        //playing with operators
+        // let f x = x
+        // let x="x"
+        // printfn "xxxxx %A %A %A"    1  (f(1))  3
+        // printfn "test0 %A %A %A"   1   x.ToString     2 
+        // printfn "test1 %A %A %A"   1   x . ToString   2  
+
+        // let ftest2  a b c =
+        //     printfn "test2 %A %A %A" a b c
+
+        // ftest2  1   x . ToString  2  
+
+        // ftest2  1   f(1)   2
+        // let inline (.)  a 0 =
+        //     a()
+        // ftest2  1   x.0  2  
+
+        // let x= "x"
+        // let f x y = printfn "%A %A" x y
+
+        // f x.ToString ()
+        // f x . ToString (1)
+
+        // let inline (~%) (a:obj) =  
+        //     sprintf "yy%A" a
+        //     // string a
+
+
+        // let inline (.) (a:obj) (b:string) =
+        //     a
+
+        // let z
+        // f %x ()
+
+        // f %System ()
+        // printfn "xxxx%A"  1+2 //x.ToString.%1
+
+
         0
 
 
--- src/Gulp/gulp.fs
+++ src/Gulp/gulp.fs
@@ -1,41 +1,98 @@
 namespace Fuze.Gulp
 
-type GulpContext()=
-    let data="context"
+open System
+type GulpContext(x)=
+
     do 
-        printfn "Gulp Context"
+        printfn "new Gulp Context"
+
+    member this.data=x
 
     member this.pipe endp =
         printfn "context pipe"
         this
 
-    member this.src globs =
-        printfn "context src"
-        this
 
-    member this.dest globs =
-        printfn "context dest"
-        this
+    // static member (|>) ((a:obj), (b:GulpContext)) =
+    //     printfn "op_BarBar"
+    //     match a with
+    //     | :?GulpContext as cxt -> cxt.pipe(b)
+    //     |  _                   -> (GulpContext(a)).pipe(b)
+    // static member op_BooleanOr ((a:obj), (b:GulpContext)) =
+    //     printfn "op_BarBar"
+    //     match a with
+    //     | :?GulpContext as cxt -> cxt.pipe(b)
+    //     |  _                   -> (GulpContext(a)).pipe(b)
+    static member (+) ((a:obj), (b:GulpContext)) =
+        printfn "op_BarBar"
+        match a with
+        | :?GulpContext as cxt -> cxt.pipe(b)
+        |  _                   -> (GulpContext(a)).pipe(b)
+    // member this.src globs =
+    //     printfn "context src"
+    //     this
+    // member this.dest globs =
+    //     printfn "context dest"
+    //     this
 
-    member this.task name deps fn =
-        printfn "context task"
-        this
+    // member this.task name deps fn =
+    //     printfn "context task"
+    //     this
+
+type GulpTaskFn = GulpContext -> GulpContext
 
 
-type Gulp() =
-    let context=GulpContext()
-    do
-        printfn "%A" context
 
-    member this.src globs  =
-        printfn "gulp.src %A" globs 
-        context.src globs
-    member this.dest globs =
-        printfn "gulp.dest %A" globs 
-        context.dest globs
-    member this.task name deps fn =
-        printfn "gulp.task %s %A %A"  name deps fn
-        context.task name deps fn
+
+type zgulp =
+
+    static member task(name, [<ParamArray>] args: System.Object[]  )=
+        // let ox = defaultArg opts (fun _ -> GulpContext())
+        // let ox = defaultArg opts (fun _ -> GulpContext())
+        // if opts
+
+        // match (opts, fn) with
+        // | ( Some (fx:gulpfn), _ )     ->
+        //                         printfn "gulp.task %s %A %A"  name deps  (fx (GulpContext())) 
+        //                         GulpContext()
+        // | ( Some (ox:obj), Some fx )  ->
+        //                         printfn "gulp.task %s %A %A %A"  name deps ox (fx (GulpContext())) 
+        //                         GulpContext()
+
+        // |  ( _, _ )   -> GulpContext()
+
+        for arg in args do
+            printfn "task %A arg %A" name arg
+            if(arg :? GulpTaskFn) then
+                // arg (  GulpContext() )
+
+                let fx=  arg :?> GulpTaskFn
+
+                let z = fx (  GulpContext() )
+                printfn "hitttttt %A" z
+
+
+            if(arg :? obj -> obj) then
+                // arg (GulpContext() )
+                printfn "hitttttt"
+
+        GulpContext()
+
+        // let fx = defaultArg fn (fun _ -> GulpContext())
+
+    static member src globs  =
+        zgulp.source globs []
+    static member dst globs =
+        zgulp.target globs []
+    static member source glps opts=
+        printfn "gulp.source %A %A" glps opts
+        GulpContext()
+    static member target glps opts=
+        printfn "gulp.target %A %A" glps opts
+        GulpContext()
+
+
+
 
 module Gulp=
     printfn "Gulp"
